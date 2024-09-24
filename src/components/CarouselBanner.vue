87824cc51ce6f490f3681cf7a3acd1ba87994095
Abhishek Negi <abhishek.fpaipl@gmail.com>
2024-09-24 19:44:17 +0530

    first commit

---
+++ src/components/CarouselBanner.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="d-md-block d-none">
+      <img src="/img/desktop.jpg" class="w-100 min-vh-100 position-relative" alt="Banner">
+      <div class="position-absolute d-flex justify-content-between align-items-center w-100 px-5" style="bottom: 10%;">
+        <a href="https://web.clgoilindia.com" class="text-capitalize btn text-white fs-4"
+          style="background-color: #FFA907;">click here to know about us</a>
+        <a href="https://www.ariseol.com" class="text-capitalize btn text-white fs-4"
+          style="background-color: #DB1D27;">click here to know
+          about ARISEOL</a>
+      </div>
+    </div>
+    <div class="d-md-none d-flex flex-column h-100 justify-content-between align-items-center w-100 min-vh-100"
+      style="background-color: black;">
+      <!-- Top Section -->
+      <div class="px-2 mt-3 w-100">
+        <img src="/img/clg.png" class="w-100 py-1" alt="clg logo">
+        <p class="text-warning fs-4 my-2">India's leading manufacturer of lubrication & grease </p>
+        <a href="https://web.clgoilindia.com" class="text-capitalize btn fs-4 rounded mb-5 mt-3"
+          style="background-color: #FFA907;">click here to know about us</a>
+      </div>
+
+      <!-- Bottom Section -->
+      <div class="w-100">
+        <img src="/img/mobile.jpg" class="w-100" alt="Banner">
+        <a href="https://www.ariseol.com" class="w-100 text-capitalize btn text-white fs-3 rounded-0"
+          style="background-color: #DB1D27;">click here to know ARISEOL</a>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  position: relative;
+  overflow: hidden;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 0, 0, 0.5);
+  /* opacity: 1; */
+}
+
+.card-title,
+.card-text {
+  position: relative;
+  /* Make sure text is above the overlay */
+  z-index: 1;
+}
+
+.card:hover .card-img {
+  transform: scale(1.05);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="">
+    <CarouselBanner />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import CarouselBanner from "@/components/CarouselBanner.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    CarouselBanner,
   },
 };
 </script>
